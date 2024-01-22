1063c92373aa44ed884481efb84d50232a09b46c
Md. Yasin Arafat <mdyasinarafathasib@gmail.com>
2024-01-22 23:48:47 +0600

    Routers and Layout added done

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,6 @@ function App() {
   return (
     <>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <button className="btn btn-primary">Primary</button>
-    
     </>
   );
 }
