d532aea13d86f4732e9d2d6847810c9d23aed823
Md. Yasin Arafat <mdyasinarafathasib@gmail.com>
2024-01-23 16:35:31 +0600

    Page Cover and New footer added

--- src/pages/Houses/Houses.jsx
+++ src/pages/Houses/Houses.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import housesCover from '../../assets/houses-cover.jpg';
+import PageCover from "../../components/PageCover";
 
 const Houses = () => {
-  return <div>Houses</div>;
+  return (
+    <div>
+      {/* Cover */}
+      <PageCover
+        bgImg={housesCover}
+        title={"Our All Houses"}
+        text={"Explore a diverse range of houses tailored to your preferences."}
+      />
+      {/* Body */}
+    </div>
+  );
 };
 
 export default Houses;
