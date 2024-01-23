00bc87f60ba2f5dd57cafd7e66d5ae68b73a7571
Md. Yasin Arafat <mdyasinarafathasib@gmail.com>
2024-01-23 11:10:08 +0600

    Commit before deploy

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Banner from "./Banner/Banner";
+import PopulerHouses from "./PopulerHouses/PopulerHouses";
 
 const Home = () => {
   return (
     <>
       <Banner />
+      <PopulerHouses />
     </>
   );
 };
---
+++ src/pages/Home/PopulerHouses/PopulerHouses.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PopulerHouses = () => {
+    return (
+        <div>
+            After Deploy
+        </div>
+    );
+};
+
+export default PopulerHouses;
