0040a0905dd73864cbc114a0ee08f8cd3251438b
Md. Yasin Arafat <mdyasinarafathasib@gmail.com>
2024-01-24 13:41:01 +0600

    Loading Spinner aded

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,9 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Outlet } from "react-router-dom";
 import Footer from "../pages/Shared/Footer/Footer";
 import Navbar from "../pages/Shared/Navbar/Navbar";
+import Spinner from "../pages/Shared/Spinner/Spinner";
 
 const Main = () => {
+    // Set loading spinner
+    const [loading, setLoading] = useState(false);
+    useEffect(() => {
+      setLoading(true);
+      setTimeout(() => {
+        setLoading(false);
+      }, 1000);
+    }, []);
+    if (loading) {
+      return <Spinner />;
+    }
+
   return (
     <div className="dark:bg-slate-800 dark:text-slate-300">
       <Navbar />
--- src/pages/Shared/Spinner/Spinner.jsx
+++ src/pages/Shared/Spinner/Spinner.jsx
@@ -1,11 +1,11 @@
-import React from 'react';
+import React from "react";
 
 const Spinner = () => {
-    return (
-        <div>
-           Spinner 
-        </div>
-    );
+  return (
+    <div className="flex items-center justify-center mt-40">
+      <div className="w-12 h-12 animate-[spin_1s_linear_infinite] rounded-full border-4 border-r-[#b6733d] border-[#5ca078]"></div>
+    </div>
+  );
 };
 
 export default Spinner;
