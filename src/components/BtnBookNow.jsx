a493e6c9caee5503ba5a88666d1af34fe06c0028
Md. Yasin Arafat <mdyasinarafathasib@gmail.com>
2024-01-24 11:48:49 +0600

    Booking Btn added with some future functionality

--- src/components/BtnBookNow.jsx
+++ src/components/BtnBookNow.jsx
@@ -1,9 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
+import toast from "react-hot-toast";
 
 const BtnBookNow = () => {
+  const [isDisabled, setIsDisabled] = useState(false);
+
+  const handleBookHouse = () => {
+    setIsDisabled(true);
+    toast.success("House Booked Successfully!");
+  };
+
   return (
     <div>
-      <button className="px-3 py-2 md:px-4  bg-[#5ca078] text-white dark:border rounded-md md:rounded-lg font-semibold md:text-base sm:text-sm text-[12px] hover:bg-[#5ebe86]">
+      <button
+        onClick={handleBookHouse}
+        disabled={isDisabled}
+        className={`px-3 py-2 md:px-4 bg-[#5ca078] text-white dark:border rounded-md md:rounded-lg font-semibold md:text-base sm:text-sm text-[12px] hover:bg-[#5ebe86] ${
+          isDisabled ? "bg-slate-300 hover:bg-slate-300 text-slate-400 cursor-not-allowed" : ""
+        }`}
+      >
         Book House
       </button>
     </div>
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { HelmetProvider } from "react-helmet-async";
+import { Toaster } from "react-hot-toast";
 import { RouterProvider } from "react-router-dom";
 import "./index.css";
 import { router } from "./routes/routes.jsx";
@@ -11,6 +12,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
       <div className="lg:max-w-screen-2xl xl:max-w-screen-xl mx-auto">
         <RouterProvider router={router} />
       </div>
+      <Toaster />
     </HelmetProvider>
   </React.StrictMode>
 );
